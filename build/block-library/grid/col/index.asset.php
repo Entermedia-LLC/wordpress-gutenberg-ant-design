9520cf4ec04b41a55299597f5badccf6c44c8815
Ben Marshall <ben.s.marshall@icloud.com>
2023-02-13 12:39:13 -0600

    build(blocks): added the post breadcrumb block

--- build/block-library/grid/col/index.asset.php
+++ build/block-library/grid/col/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '763653b9f864f7489b44');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c3ae788461e48599eb29');
