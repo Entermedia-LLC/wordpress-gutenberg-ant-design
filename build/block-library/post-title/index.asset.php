2b8adc885391407072e6f274727f3403b8e8255f
Ben Marshall <ben.s.marshall@icloud.com>
2023-02-15 09:19:15 -0600

    build(post title): updated the compiled code to prepare to fix conflicts

--- build/block-library/post-title/index.asset.php
+++ build/block-library/post-title/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7d64fc6854926cb35a8d');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'daf6ceddad256f967b24');
