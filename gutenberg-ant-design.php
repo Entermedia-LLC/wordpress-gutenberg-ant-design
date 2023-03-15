ee7420586f95e52a174ab011b827e63eeef6923a
Ben Marshall <ben.s.marshall@icloud.com>
2023-03-15 12:44:23 -0500

    build(cagle): removed client specific component

--- gutenberg-ant-design.php
+++ gutenberg-ant-design.php
@@ -40,7 +40,6 @@ function gutenberg_ant_design_register_blocks() {
 	register_block_type( __DIR__ . '/build/block-library/post-featured-image' );
 	register_block_type( __DIR__ . '/build/block-library/list/item' );
 	register_block_type( __DIR__ . '/build/block-library/list/list' );
-	register_block_type( __DIR__ . '/build/block-library/cagle-team-members' );
 }
 add_action( 'init', 'gutenberg_ant_design_register_blocks' );
 
